8c6a39dcf8a0c8f72eb44d82dc0a3f09e8cc25cc
Brandon <brandon.bl.langlois@gmail.com>
2025-10-01 21:44:32 -0400

    until 27

---
+++ MapTester.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+/**
+ * Write a description of class MapTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MapTester
+
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class MapTester
+     */
+    public MapTester()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+    public void enterNumber(String name, String number)
+    {
+        
+    }
+    
+    public String lookupNumber(String name)
+    {
+        
+    }
+    
+    HashMap<String, String> contacts = new HashMap<>();
+    contacts.put("Charles Nguyen", "(531) 9392 4587");
+    contacts.put("Lisa Jones", "(402) 4536 4674");
+    contacts.put("Hiroshi Ito", "(998) 5488 0123");
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
